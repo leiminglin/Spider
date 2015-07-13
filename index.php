8c73772656c21f367c1cdca9a221891b61d3193a
leiminglin <leiminglin@126.com>
2015-07-14 00:40:21 +0800

    modify remote lib

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 function getRemoteLmlPhp(){
 	$cache_filename = 'lml.min.php';
-	$remotelib = 'http://pro8091d8.pic12.websiteonline.cn/upload/lmlphp-release-2014-08-27-v1.txt';
+	$remotelib = 'http://git.oschina.net/leiminglin/LMLPHP/raw/master/lml.min.php';
 	if( file_exists( $cache_filename ) ) {
 		$cachemtime = filemtime($cache_filename);
 		if( $cachemtime + 86400 > time() ){
